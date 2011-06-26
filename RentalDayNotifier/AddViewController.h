8e84a4b7ed75156db2951143a712f0e7c171a505
giginet <giginet.net@gmail.com>
2011-06-26 20:13:24 +0900

    ひな形まで作成

---
+++ RentalDayNotifier/AddViewController.h
@@ -0,0 +1,16 @@
+//
+//  AddViewController.h
+//  RentalDayNotifier
+//
+//  Created by giginet on 11/06/26.
+//  Copyright 2011 Kawaz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface AddViewController : UITableViewController {
+    
+}
+
+@end
