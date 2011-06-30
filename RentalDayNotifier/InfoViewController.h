0b8d3b2996b57cd4b861a81a2e32ad2f14897200
giginet <giginet.net@gmail.com>
2011-06-30 15:05:49 +0900

    テンプレートまで作成

---
+++ RentalDayNotifier/InfoViewController.h
@@ -0,0 +1,16 @@
+//
+//  InfoViewController.h
+//  RentalDayNotifier
+//
+//  Created by giginet on 11/06/26.
+//  Copyright 2011 Kawaz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface InfoViewController : UIViewController {
+    
+}
+
+@end
---
+++ RentalDayNotifier/SettingViewController.h
@@ -0,0 +1,16 @@
+//
+//  SettingViewController.h
+//  RentalDayNotifier
+//
+//  Created by giginet on 11/06/26.
+//  Copyright 2011 Kawaz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface SettingViewController : UITableViewController {
+    
+}
+
+@end
